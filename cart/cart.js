1911e12fada89bace38c8c94d5599ba0f92b80c6
Deepaksahu029 <100663934+Deepaksahu029@users.noreply.github.com>
2022-03-06 18:48:24 +0530

    cart

--- cart/cart.js
+++ cart/cart.js
@@ -1,41 +1,84 @@
+let amt_val=1
+let total_price
+let totall
 getitem()
 function getitem(){
     let data= JSON.parse(localStorage.getItem("tocart"))
+
     displayitemincart(data)
 }
+
 function displayitemincart(data){
     // console.log(data)
     let op=""
     let  disinparent=document.getElementById("qun")
         
-     
-    data.forEach(e => {
-        // let parentdiv=document.createElement("div")
-        // parentdiv.setAttribute("id","parentdiv")
-        // let img_tit
-        op+=`<div id="parentdiv">
-        <div id="image_title">
-            <img src=${e.img} alt="">
-            <p>${e.title}</p>
-        </div>
-        <div id="qunt">
-        <select name="" id="qun_amt">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-</select>
-        </div>
   
+    data.forEach(e => {
+    
+let parentdiv=document.createElement("div")
+parentdiv.setAttribute("id","parentdiv")
+let image_title=document.createElement("div")
+image_title.setAttribute("id","image_title")
+let img=document.createElement("img")
+img.src=e.img;
+let p=document.createElement("p")
+p.textContent=e.title
+image_title.append(img,p)
+let qunt=document.createElement("div")
+qunt.setAttribute("id","qunt")
+let qun_amt=document.createElement("select")
+qun_amt.setAttribute("id","qun_amt")
+
+
+let opt1=document.createElement("option")
+opt1.value=1
+opt1.textContent=1
+let opt2=document.createElement("option")
+opt2.value=2
+opt2.textContent=2
+let opt3=document.createElement("option")
+opt3.value=3
+opt3.textContent=3
+let opt4=document.createElement("option")
+opt4.value=4
+opt4.textContent=4
+let opt5=document.createElement("option")
+opt5.value=5
+opt5.textContent=5
+qun_amt.append(opt1,opt2,opt3,opt4,opt5)
+qunt.append(qun_amt)
+
+
+
+ let price=document.createElement("div")
+ price.setAttribute("id","pricee")
+price.textContent="$"+e.price
+let opp=parseFloat(e.price)
+
+let total=document.createElement("div")
+total.setAttribute("id","total_val")
+qun_amt.addEventListener("change",function(e){
+    amt_val=parseInt(e.target.value)
     
     
-        
-        <div id="price">$${e.price}</div>
-        <div id="total">${e.price*document.getElementById("qun_amt").value}</div>
-        </div>`
-        
+    
+totall=amt_val*opp
+
+var tt=document.getElementById("total_val")
+tt.textContent=totall
+})
+
+
+// console.log(totall)
+// total.textContent= "$"+totall
+parentdiv.append(image_title,qunt,price,total)
+disinparent.append(parentdiv)
     });
-    disinparent.innerHTML=op
+    // disinparent.innerHTML=op
    
 }
+// function total_name(e,val){
+// let t=document.getElementById(e.id)
+// t.innerText=e.price*val
+// }
